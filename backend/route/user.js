a54e670daed5118eda1cc6ebbc1e710eaf700e76
TheisFlorian <theisflorian99@gmail.com>
2022-09-21 14:05:40 +0200

    fin du projet

--- backend/route/user.js
+++ backend/route/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const rateLimit = require('../middleware/limiter')
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', rateLimit.limiter, userCtrl.signup);
+router.post('/login', rateLimit.limiter, userCtrl.login);
 
 module.exports = router;
